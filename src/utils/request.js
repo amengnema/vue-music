26108e373bc2eec6aac91a268265be20ffe69dec
zm <zm@uiui.padavan>
2022-02-24 16:26:27 +0800

    修改api链接

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,9 @@ export function request(config) {
   const instance = axios.create({
     // baseURL:'http://localhost:5005',
     // baseURL:'http://localhost:3000',
-    baseURL:'https://netease-music-api.fe-mm.com',
+    // baseURL:'https://netease-music-api.fe-mm.com',
+    //20220224更新：新的api链接
+    baseURL:'https://netease-cloud-music-api-chi-navy.vercel.app',
     timeout:5000
   })
   //得在这里设置
